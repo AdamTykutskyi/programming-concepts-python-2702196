eb58ba7db235b734ddbe916a37dd60eb74ac6222
AdamTykutskyi <stud.tykutskyi@gdromagnosi.edu.it>
2026-03-19 09:49:50 +0000

    prova1

--- examples/01_01_functions/functions_begin.py
+++ examples/01_01_functions/functions_begin.py
@@ -1,8 +1,17 @@
 """ A Functional Breakfast """
 
-print('Mixing the ingredients')
-print('Pouring the mixture into a frying pan')
-print('Cooking the first side')
-print('Flipping it!')
-print('Cooking the other side\n')
-omelette = 'a tasty omelette'
+
+
+def make_omelette():
+    print('Mixing the ingredients')
+    print('Pouring the mixture into a frying pan')
+    print('Cooking the first side')
+    print('Flipping it!')
+    print('Cooking the other side\n')
+    omelette = 'a tasty omelette'
+    return omelette
+#fare per due
+barron_breakfast = make_omelette()
+olivia_breakfast = make_omelette()
+print(f'Adam is having {adam_breakfast}\n')
+print(f'Ale is having {ale_breakfast}\n')
